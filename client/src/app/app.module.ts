45b52159c60a267e1b222ce8d8a18fbe4266359b
Daniele Moscati <d.moscati@nbsgroup.it>
2021-12-22 10:26:36 +0100

    Added simple login checks

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
@@ -11,6 +13,9 @@ import { MenubarModule } from 'primeng/menubar';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';
+
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -20,15 +25,17 @@ import { CardModule } from 'primeng/card';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     FormsModule,
 
     SharedModule,
     MenubarModule,
     InputTextModule,
     ButtonModule,
-    CardModule
+    CardModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/components/homepage/homepage.component.ts
+++ client/src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-homepage',
@@ -10,9 +11,37 @@ export class HomepageComponent implements OnInit {
   public username: string = "";
   public password: string = "";
 
-  constructor() { }
+  constructor( private messageService: MessageService ) { }
 
   ngOnInit(): void {
+  }
+
+  checkInputs(): boolean {
+    let errorAmount = 0;
+    let errorMessage = "";
+
+    if (this.username === "") {
+      errorAmount++;
+      errorMessage += errorAmount + " - Username cannot be empty \n";
+    }
+
+    if (this.password === "") {
+      errorAmount++;
+      errorMessage += errorAmount + " - Password cannot be empty \n";
+    }
+
+    if (errorAmount > 0) {
+      this.messageService.add({severity:'error', summary: 'Error', detail: errorMessage});
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  login(): void {
+    if (this.checkInputs() === true) {
+      //TODO: Fare Login
+    }
   }
 
 }
